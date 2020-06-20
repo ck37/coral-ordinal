64a82e6d8a89780f1729dbcd8750ad61ce1c0626
Chris Kennedy <chrisken@gmail.com>
2020-06-20 16:30:52 -0700

    see about updating setup.py

--- setup.py
+++ setup.py
@@ -13,18 +13,16 @@ with open(os.path.join(_here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name = 'coral_ordinal',
+    name = 'coral-ordinal-ck37',
     url = 'https://github.com/ck37/coral_ordinal',
     author = 'Chris Kennedy',
     author_email = 'chrisken@gmail.com',
-    # Needed to actually package something
-    packages = ['coral_ordinal'],
-    # Needed for dependencies
+    packages = setuptools.find_packages(),
     install_requires = ['numpy', 'pandas', 'tensorflow', 'scipy'],
     version = version['__version__'],
     long_description_content_type = "text/markdown",
-    # The license can be anything you like
     license = 'MIT',
     description = 'TF.Keras implementation of CORAL ordinal classification output layer',
-    long_description = long_description
+    long_description = long_description,
+    python_requires = '>=3.6',
 )
