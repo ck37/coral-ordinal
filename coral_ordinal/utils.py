ef26473a82f65552c84da6dfa04be73ca643e48e
Chris Kennedy <chrisken@gmail.com>
2020-06-20 17:06:55 -0700

    fix logits_to_probs

--- coral_ordinal/utils.py
+++ coral_ordinal/utils.py
@@ -6,7 +6,7 @@ def logits_to_probs(preds, num_classes):
   preds = pd.DataFrame(preds)
  
   # Create a new dataframe to store the probabilities.
-  probs = pd.DataFrame(0., index = range(preds.shape[0]), columns = range(NUM_CLASSES))
+  probs = pd.DataFrame(0., index = range(preds.shape[0]), columns = range(num_classes))
 
   # First, get probability predictions out of the cumulative logits.
   # Column 0 is Probability that y > 0, so Pr(y = 0) = 1 - Pr(y > 0)
@@ -19,7 +19,7 @@ def logits_to_probs(preds, num_classes):
   # For the other columns, the probability is:
   # Pr(y = k) = Pr(y > k) - Pr(y > k - 1)
   if num_classes > 2:
-    for val in range(1, NUM_CLASSES - 1):
+    for val in range(1, num_classes - 1):
       probs[val] = special.expit(preds[val - 1]) - special.expit(preds[val])
   
   return probs
