dcbc4792a07e81ca63270a1fc7c30a820307c504
Chris Kennedy <chrisken@gmail.com>
2020-06-23 09:05:25 -0700

    mae fixes

--- coral_ordinal/metrics.py
+++ coral_ordinal/metrics.py
@@ -7,7 +7,9 @@ def MeanAbsoluteErrorLabels(y_true, y_pred):
   cum_probs = tf.map_fn(tf.math.sigmoid, y_pred)
   
   # Calculate the labels using the style of Cao et al.
-  labels_v2 = tf.reduce_sum(tf.map_fn(lambda x: x > 0.5, cum_probs), axis = 1)
+  labels_v2 = tf.reduce_sum(tf.map_fn(lambda x: tf.cast(x > 0.5, tf.int16), cum_probs), axis = 1)
+  
+  labels_v2 = tf.cast(labels_v2, y_true.dtype)
   
   return tf.reduce_mean(tf.abs(y_true - labels_v2))
 
