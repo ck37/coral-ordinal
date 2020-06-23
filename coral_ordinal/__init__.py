82ae7d5988226b4c148c90aec751f6d5a6eb77f1
Chris Kennedy <chrisken@gmail.com>
2020-06-23 13:39:37 -0700

    init: export ordinal_softmax

--- coral_ordinal/__init__.py
+++ coral_ordinal/__init__.py
@@ -4,6 +4,7 @@ from .layer import CoralOrdinal
 from .loss import OrdinalCrossEntropy
 from .utils import logits_to_probs
 from .metrics import MeanAbsoluteErrorLabels
+from .activations import ordinal_softmax
 
 # if somebody does "from somepackage import *", this is what they will
 # be able to access:
@@ -11,5 +12,6 @@ __all__ = [
   'CoralOrdinal',
   'MeanAbsoluteErrorLabels',
   'OrdinalCrossEntropy',
+  'ordinal_softmax',
   'logits_to_probs',
 ]
