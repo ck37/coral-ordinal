fix layer and losses for weighting and serialization

coral_ordinal/layer.py
@@ -1,5 +1,7 @@
+from typing import Optional
+import warnings
 import tensorflow as tf
-from tensorflow.python.keras import activations
+import tensorflow.keras.regularizers
 
 
 @tf.keras.utils.register_keras_serializable(package="coral_ordinal")
@@ -8,7 +10,14 @@ class CoralOrdinal(tf.keras.layers.Layer):
 
     # We skip input_dim/input_shape here and put in the build() method as recommended in the tutorial,
     # in case the user doesn't know the input dimensions when defining the model.
-    def __init__(self, num_classes: int, activation=None, **kwargs):
+    def __init__(
+        self,
+        num_classes: int,
+        activation: Optional[str] = None,
+        kernel_regularizer: Optional[tf.keras.regularizers.Regularizer] = None,
+        bias_regularizer: Optional[tf.keras.regularizers.Regularizer] = None,
+        **kwargs,
+    ):
         """Ordinal output layer, which produces ordinal logits by default.
 
         Args:
@@ -16,6 +25,9 @@ class CoralOrdinal(tf.keras.layers.Layer):
           activation: (Optional) Activation function to use. The default of None produces
             ordinal logits, but passing "ordinal_softmax" will cause the layer to output
             a probability prediction for each label.
+          kernel_regularizer: regularizer for kernel of Coral Dense layer.
+          bias_regularizer: regularizer for bias of Coral Dense layer.
+          **kwargs: keyword arguments passed to Layer().
         """
 
         # Via Dense Layer code:
@@ -26,7 +38,9 @@ class CoralOrdinal(tf.keras.layers.Layer):
         # Pass any additional keyword arguments to Layer() (i.e. name, dtype)
         super(CoralOrdinal, self).__init__(**kwargs)
         self.num_classes = num_classes
-        self.activation = activations.get(activation)
+        self.activation = tf.keras.activations.get(activation)
+        self.kernel_regularizer = tf.keras.regularizers.get(kernel_regularizer)
+        self.bias_regularizer = tf.keras.regularizers.get(bias_regularizer)
 
     # Following https://www.tensorflow.org/guide/keras/custom_layers_and_models#best_practice_deferring_weight_creation_until_the_shape_of_the_inputs_is_known
     def build(self, input_shape):
@@ -42,6 +56,7 @@ class CoralOrdinal(tf.keras.layers.Layer):
             # Need a unique name if there are multiple coral_ordinal layers.
             name=self.name + "_latent",
             initializer="glorot_uniform",
+            regularizer=self.kernel_regularizer,
             # Not sure if this is necessary:
             dtype=tf.float32,
             trainable=True,
@@ -52,6 +67,7 @@ class CoralOrdinal(tf.keras.layers.Layer):
             shape=(self.num_classes - 1,),
             # Need a unique name if there are multiple coral_ordinal layers.
             name=self.name + "_bias",
+            regularizer=self.bias_regularizer,
             initializer="zeros",
             # Not sure if this is necessary:
             dtype=tf.float32,
@@ -72,11 +88,17 @@ class CoralOrdinal(tf.keras.layers.Layer):
 
         return outputs
 
-    # This allows for serialization supposedly.
+    # This allows for serialization.
     # https://www.tensorflow.org/guide/keras/custom_layers_and_models#you_can_optionally_enable_serialization_on_your_layers
     def get_config(self):
         config = super(CoralOrdinal, self).get_config()
-        config.update({"num_classes": self.num_classes})
+        config.update(
+            {
+                "num_classes": self.num_classes,
+                "kernel_regularizer": self.kernel_regularizer,
+                "bias_regularizer": self.bias_regularizer,
+            }
+        )
         return config
 
 
@@ -95,6 +117,10 @@ class CornOrdinal(tf.keras.layers.Dense):
             ordinal logits, but passing "ordinal_softmax" will cause the layer to output
             a probability prediction for each label.
         """
+        if "units" in kwargs:
+            warnings.warn("Use 'num_classes' instead of 'units'. Dropping ...")
+            kwargs.pop("units")
+
         super(CornOrdinal, self).__init__(
             units=num_classes - 1, activation=activation, **kwargs
         )

coral_ordinal/loss.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+import warnings
 import functools
 import tensorflow as tf
 import numpy as np
@@ -29,14 +30,14 @@ def _ordinal_loss_no_reduction(
 
 
 # The outer function is a constructor to create a loss function using a certain number of classes.
-@tf.keras.utils.register_keras_serializable(package="coral_ordinal")
+# @tf.keras.utils.register_keras_serializable(package="coral_ordinal")
 class OrdinalCrossEntropy(tf.keras.losses.Loss):
     """Computes ordinal cross entropy based on ordinal predictions and outcomes."""
 
     def __init__(
         self,
         num_classes: Optional[int] = None,
-        importance_weights=None,
+        importance_weights: Optional[np.ndarray] = None,
         from_type: str = "ordinal_logits",
         name: str = "ordinal_crossentropy",
         **kwargs,
@@ -46,11 +47,12 @@ class OrdinalCrossEntropy(tf.keras.losses.Loss):
         Args:
           num_classes: number of ranks (aka labels or values) in the ordinal variable.
             This is optional; can be inferred from size of y_pred at runtime.
-          importance_weights: (Optional) importance weights for each binary classification task.
+          importance_weights: class weights for each binary classification task.
           from_type: one of "ordinal_logits" (default), "logits", or "probs".
             Ordinal logits are the output of a CoralOrdinal() layer with no activation.
             (Not yet implemented) Logits are the output of a dense layer with no activation.
-            (Not yet implemented) Probs are the probability outputs of a softmax or ordinal_softmax layer.
+            (Not yet implemented) Probs are the probability outputs of a softmax or ordinal_softmax
+            layer.
           name: name of layer
           **kwargs: keyword arguments passed to Loss().
         """
@@ -61,7 +63,12 @@ class OrdinalCrossEntropy(tf.keras.losses.Loss):
         self.from_type = from_type
 
     # Following https://www.tensorflow.org/api_docs/python/tf/keras/losses/Loss
-    def call(self, y_true: tf.Tensor, y_pred: tf.Tensor):
+    def call(
+        self,
+        y_true: tf.Tensor,
+        y_pred: tf.Tensor,
+        sample_weight: Optional[tf.Tensor] = None,
+    ):
 
         # Ensure that y_true is the same type as y_pred (presumably a float).
         y_pred = tf.convert_to_tensor(y_pred)
@@ -89,17 +96,11 @@ class OrdinalCrossEntropy(tf.keras.losses.Loss):
                 + self.from_type
                 + " in OrdinalCrossEntropy()"
             )
-        if self.reduction == tf.keras.losses.Reduction.NONE:
-            return loss
-        elif self.reduction in [
-            tf.keras.losses.Reduction.AUTO,
-            tf.keras.losses.Reduction.SUM_OVER_BATCH_SIZE,
-        ]:
-            return tf.reduce_mean(loss)
-        elif self.reduction == tf.keras.losses.Reduction.SUM:
-            return tf.reduce_sum(loss)
-        else:
-            raise Exception(f"{self.reduction} is not a valid reduction.")
+
+        if sample_weight is not None:
+            loss *= sample_weight
+
+        return loss
 
     def get_config(self):
         config = {
@@ -111,30 +112,30 @@ class OrdinalCrossEntropy(tf.keras.losses.Loss):
         return {**base_config, **config}
 
 
-@tf.keras.utils.register_keras_serializable(package="coral_ordinal")
+# @tf.keras.utils.register_keras_serializable(package="coral_ordinal")
 class CornOrdinalCrossEntropy(tf.keras.losses.Loss):
     """Implements CORN ordinal loss function for logits."""
 
     def __init__(
         self,
-        reduction: tf.keras.losses.Reduction = tf.keras.losses.Reduction.AUTO,
         **kwargs,
     ):
         """Initializes class."""
-        if not reduction in [
-            tf.keras.losses.Reduction.AUTO,
-            tf.keras.losses.Reduction.SUM_OVER_BATCH_SIZE,
-        ]:
-            raise NotImplementedError(f"reduction={reduction} is not implemented.")
-        super().__init__(reduction=reduction, **kwargs)
+        super().__init__(**kwargs)
         self.num_classes = None
 
-    def call(self, y_true: tf.Tensor, y_pred: tf.Tensor):
+    def __call__(
+        self,
+        y_true: tf.Tensor,
+        y_pred: tf.Tensor,
+        sample_weight: Optional[tf.Tensor] = None,
+    ):
         """Computes the CORN loss described in https://arxiv.org/abs/2111.08851
 
         Args:
           y_true: true labels (0..N-1)
           y_pred: predicted logits (from CornLayer())
+          sample_weights: optional; provide sample weights for each sample.
 
         Returns:
           loss: tf.Tensor, that contains the loss value.
@@ -152,7 +153,7 @@ class CornOrdinalCrossEntropy(tf.keras.losses.Loss):
             sets.append((set_mask, label_gt_i))
 
         n_examples = []
-        losses = 0.0
+        losses = tf.zeros_like(y_true)
         for task_index, s in enumerate(sets):
             set_mask, label_gt_i = s
             n_examples_task = tf.reduce_sum(tf.cast(set_mask, dtype=tf.float32))
@@ -170,6 +171,10 @@ class CornOrdinalCrossEntropy(tf.keras.losses.Loss):
                 ),
                 0.0,  # don't add to loss if label is <= i - 1
             )
-            losses += -tf.reduce_sum(losses_task)
+            losses += -losses_task
+        losses /= self.num_classes
 
-        return losses / self.num_classes
+        if sample_weight is not None:
+            losses *= sample_weight
+
+        return losses

coral_ordinal/tests/test_loss.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 import numpy as np
 import pytest
 
-from coral_ordinal import loss as coral_loss
+from coral_ordinal import loss
 from coral_ordinal import layer
 
 
@@ -16,12 +16,13 @@ def _create_test_data():
 
     X = np.random.normal(size=(8, 99))
     y = np.array([0, 1, 2, 2, 2, 3, 4, 4])
-    return X, y
+    sample_weights = np.ndarray([0, 1, 1, 1, 1, 1, 1, 1])
+    return X, y, sample_weights
 
 
 def test_corn_loss():
-    X, y = _create_test_data()
-    corn_loss = coral_loss.CornOrdinalCrossEntropy()
+    X, y, _ = _create_test_data()
+    corn_loss = loss.CornOrdinalCrossEntropy()
     num_classes = len(np.unique(y))
     tf.random.set_seed(1)
     corn_net = layer.CornOrdinal(num_classes=num_classes, input_dim=X.shape[1])
@@ -32,3 +33,60 @@ def test_corn_loss():
     # see https://github.com/Raschka-research-group/coral-pytorch/blob/main/coral_pytorch/losses.py
     # for approximately same value for pytorch immplementation.
     assert loss.numpy() == pytest.approx(3.54, 0.01)
+
+
+# ("auto", "none", "sum", "sum_over_batch_size")
+
+
+@pytest.mark.parametrize(
+    "reduction,expected_len",
+    [("auto", 1), ("none", 8), ("sum", 1), ("sum_over_batch_size", 1)],
+)
+def test_coral_loss_reduction(reduction, expected_len):
+    X, y, _ = _create_test_data()
+    coral_loss = loss.OrdinalCrossEntropy(reduction=reduction)
+    num_classes = len(np.unique(y))
+
+    tf.random.set_seed(1)
+    coral_net = layer.CoralOrdinal(num_classes=num_classes, input_dim=X.shape[1])
+    logits = coral_net(X)
+    loss_val = coral_loss(y_true=y, y_pred=logits)
+    if expected_len == 1:
+        assert loss_val.numpy() > 0
+    else:
+        assert loss_val.shape[0] == expected_len
+
+
+@pytest.mark.parametrize(
+    "reduction,expected_len",
+    [("auto", 1), ("none", 8), ("sum", 1), ("sum_over_batch_size", 1)],
+)
+def test_corn_loss_reduction(reduction, expected_len):
+    X, y, _ = _create_test_data()
+    corn_loss = loss.CornOrdinalCrossEntropy(reduction=reduction)
+    num_classes = len(np.unique(y))
+
+    tf.random.set_seed(1)
+    corn_net = layer.CornOrdinal(num_classes=num_classes, input_dim=X.shape[1])
+    logits = corn_net(X)
+    loss_val = corn_loss(y_true=y, y_pred=logits)
+    if expected_len == 1:
+        assert loss_val.numpy() > 0
+    else:
+        assert loss_val.shape[0] == expected_len
+
+
+def test_sample_weights_loss():
+    X, y, sample_weights = _create_test_data()
+    corn_loss = loss.CornOrdinalCrossEntropy(reduction="none")
+    num_classes = len(np.unique(y))
+
+    tf.random.set_seed(1)
+    corn_net = layer.CornOrdinal(num_classes=num_classes, input_dim=X.shape[1])
+    logits = corn_net(X)
+    loss_val = corn_loss(y_true=y, y_pred=logits)
+
+    loss_val_weighted = corn_loss(y_true=y, y_pred=logits, sample_weight=sample_weights)
+
+    print(loss_val, loss_val_weighted)
+    assert False